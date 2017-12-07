3efc46f03fb608a3d62687a4b762ca523c07ddbb
grossherr <32869105+grossherr@users.noreply.github.com>
2017-12-07 13:42:16 +0100

    Add files via upload

---
+++ functions-fileloader.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ Plugin Name:  Functions Fileloader
+ Plugin URI:   https://github.com/grossherr/functions-fileloader
+ GitHub Plugin URI: grossherr/functions-fileloader
+ Description:  WordPress Plugin for loading files from the functions.php
+ Version:      0.5
+ Author:       Nicolai
+ Author URI:   https://ngcorp.de
+ License:      GPL3
+ License URI:  https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+//Load Class
+require_once( dirname( __FILE__ ) . '/includes/class-file-loader.php' );
+//Load Function
+require_once( dirname( __FILE__ ) . '/includes/function-file-loader.php' );
